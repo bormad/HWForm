e9ce4575132465b93304279562616050c57fea06
Vova <tarabanvv@mail.ru>
2023-12-06 12:09:05 +0800

    add react-form

--- src/App.js
+++ src/App.js
@@ -1,90 +1,66 @@
 import './App.css';
 import React from 'react';
 import { userValidation } from './validations';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
 
 export const App = () => {
-	const [inputValue, setInputValue] = React.useState({
-		email: '',
-		password: '',
-		repeatPassword: ''
-	});
-	const [error, setError] = React.useState(true);
-	const submitButtonRef = React.useRef(null);
-
-	const onChangeInput = (key, value) => {
-		setInputValue(prev => ({ ...prev, [key]: value }));
-	};
-
-	const onClickBtn = event => {
-		event.preventDefault();
-		console.log(inputValue);
-		setInputValue({
+	const [error, setError] = React.useState('');
+	const {
+		register,
+		handleSubmit,
+		formState: { errors }
+	} = useForm({
+		defaultValues: {
 			email: '',
 			password: '',
 			repeatPassword: ''
-		});
-		setError(true);
+		},
+		resolver: yupResolver(userValidation)
+	});
+
+	const sendFormData = formData => {
+		console.log(formData);
 	};
 
 	React.useEffect(() => {
-		const validateInput = async () => {
-			try {
-				await userValidation.validate(inputValue);
-				setError(null);
-			} catch (error) {
-				setError(error.message);
-			}
-		};
-
-		validateInput();
-	}, [inputValue]);
-
-	React.useEffect(() => {
-		if (!error) {
-			submitButtonRef.current.focus();
-		}
-	}, [error]);
-
+		setError(
+			errors.email?.message ||
+				errors.password?.message ||
+				errors.repeatPassword?.message ||
+				''
+		);
+	}, [errors]);
 	return (
 		<div className='App'>
-			<form className='App__form'>
+			<form className='App__form' onSubmit={handleSubmit(sendFormData)}>
 				<input
+					{...register('email')}
 					name='email'
-					value={inputValue.email}
-					onChange={({ target }) => onChangeInput('email', target.value)}
 					className='App__input'
 					type='text'
 					placeholder='Email'
 				/>
 				<input
+					{...register('password')}
 					name='password'
-					value={inputValue.password}
-					onChange={({ target }) => onChangeInput('password', target.value)}
 					className='App__input'
 					type='password'
 					placeholder='Password'
 				/>
 				<input
+					{...register('repeatPassword')}
 					name='repeatPassword'
-					value={inputValue.repeatPassword}
-					onChange={({ target }) =>
-						onChangeInput('repeatPassword', target.value)
-					}
 					className='App__input'
 					type='password'
 					placeholder='Repeat password'
 				/>
-				<button
-					className='App_btn'
-					ref={submitButtonRef}
-					onClick={onClickBtn}
-					type='submit'
-					disabled={!!error}
-				>
+
+				<button className='App_btn' type='submit'>
 					Зарегистрироваться
 				</button>
 			</form>
-			{error && <div className='App__error'>{error}</div>}
+			<div className='App__error'>{error && <div>{error}</div>}</div>
 		</div>
 	);
 };
